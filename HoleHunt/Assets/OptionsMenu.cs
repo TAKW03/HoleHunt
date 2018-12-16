31db28ee2cbe32f444f237290e0634c40b260e8e
TAKW03 <n03ytm@gmail.com>
2018-12-16 20:14:12 +0100

    Jobbet med Menyen

---
+++ HoleHunt/Assets/OptionsMenu.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour {
+
+    public AudioMixer audioMixer;
+
+    public Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+
+    void start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+        }
+
+        resolutionDropdown.AddOptions(options);
+
+    }
+
+    public void SetVolume (float MainVolume)
+    {
+        audioMixer.SetFloat("MainVolume", MainVolume);
+    }
+
+
+    public void SetMusicVolume(float MusicVolume)
+    {
+        audioMixer.SetFloat("MusicVolume", MusicVolume);
+    }
+
+    public void SetQuality (int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen (bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+}
