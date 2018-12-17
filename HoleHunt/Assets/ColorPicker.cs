1207abe2339aa9a47e83be77cf23e07477889130
TAKW03 <n03ytm@gmail.com>
2018-12-17 15:49:33 +0100

    Ferdi med fargevelger

---
+++ HoleHunt/Assets/ColorPicker.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorPicker : MonoBehaviour
+{
+
+    public Material[] BodyColorMat;
+    Material CurrMat;
+    Renderer renderer;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        renderer = this.GetComponent<Renderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //render blue color
+    public void BlueColor()
+    {
+        renderer.material = BodyColorMat[0];
+        CurrMat = renderer.material;
+    }
+
+    //render red color
+    public void RedColor()
+    {
+        renderer.material = BodyColorMat[1];
+        CurrMat = renderer.material;
+    }
+
+    //render greencolor
+    public void GreenColor()
+    {
+        renderer.material = BodyColorMat[2];
+        CurrMat = renderer.material;
+    }
+
+
+    //render yellow color
+    public void YellowColor()
+    {
+        renderer.material = BodyColorMat[3];
+        CurrMat = renderer.material;
+    }
+
+    //render pink color
+    public void PinkColor()
+    {
+        renderer.material = BodyColorMat[4];
+        CurrMat = renderer.material;
+    }
+
+    //render orange color
+    public void OrangeColor()
+    {
+        renderer.material = BodyColorMat[5];
+        CurrMat = renderer.material;
+    }
+
+    //render black color
+    public void BlackColor()
+    {
+        renderer.material = BodyColorMat[6];
+        CurrMat = renderer.material;
+    }
+
+    //render white color
+    public void WhiteColor()
+    {
+        renderer.material = BodyColorMat[7];
+        CurrMat = renderer.material;
+    }
+}
